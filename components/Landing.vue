3c7ca7744aa847778a31cbf5a754e26a047a2ca7
aurelien.hmdy <aurelien@hemidy.fr>
2022-10-12 15:31:57 +0200

    :fire: Cleaning landing page and directory

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,48 +1,23 @@
 <template>
-  <GridLanding>
-    <MainTitle />
+  <LandingGrid>
+    <LandingMainTitle />
 
-    <ImageLanding :onHover="() => (showlandingAnimation = !showlandingAnimation)" />
+    <LandingImage :onHover="() => (showlandingAnimation = !showlandingAnimation)" />
 
-    <ProjectsTitleLanding :isAnimating="showlandingAnimation" />
+    <LandingProjectsTitle :isAnimating="showlandingAnimation" />
 
-    <ViewTextLanding :isAnimating="showlandingAnimation" />
+    <LandingViewText :isAnimating="showlandingAnimation" />
 
-    <DateLanding />
+    <LandingDate />
 
-    <BottomLeftButton />
+    <LandingBottomLeftButton />
 
-    <BottomLineLanding />
+    <LandingBottomLine />
 
     <BackgroundLines />
-  </GridLanding>
+  </LandingGrid>
 </template>
 
 <script setup>
-import gsap from 'gsap';
-
-const rightLine = ref(null);
-const rightLineText = ref(null);
-const projectTextLeft = ref(null);
-const image = ref(null);
-
 const showlandingAnimation = ref(false);
-
-// const
-
-onMounted(() => {
-  // gsap.to(image.value, {
-  //   '--scaleX': 0,
-  //   duration: 0.8,
-  //   ease: 'cubic-bezier(0.62, 0.05, 0.01, 0.99)',
-  // });
-});
-
-const onMouseEnterImage = (e) => {
-  tlRightLine.play();
-};
-
-const onMouseLeaveImage = (e) => {
-  tlRightLine.reverse();
-};
 </script>
